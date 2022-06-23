a437c9544f0bc82781a41323ecf9450eda1c998e
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-23 17:23:22 -0400

    dropped and created database company_db in schema.sql; created table department in company_db

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the company_db if it exists currently --
+DROP DATABASE IF EXISTS company_db;
+-- Creates the company_db database --
+CREATE DATABASE company_db;
+
+-- Use company_db database --
+USE company_db;
+ 
+-- Creates the table "department" within company_db --
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+SHOW TABLES;
