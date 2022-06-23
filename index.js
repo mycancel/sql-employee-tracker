6655d0d0c239b6fc08627dd24b9c03fe51fc0f65
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-23 18:09:15 -0400

    added query to select all info from department into viewDepartments(); adjusted wording in comment about viewDepartments()

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2');
-const { DEC8_BIN } = require('mysql2/lib/constants/charsets');
 
 // Creates connection to the database
 // TODO: Create this into a global variable? Or keep as a function?
@@ -16,10 +15,12 @@ const getConnection = () => {
     return companyDb;
 };
 
-// Returns information from table department
-const viewDepartments = async () => { 
+// Recieves information from table department
+const viewDepartments = () => { 
     const companyDb = getConnection();
-    console.log(companyDb);
+    companyDb.query('SELECT * FROM department', (err, results) => {
+        console.log(results);
+    })
 };
 
 viewDepartments();
