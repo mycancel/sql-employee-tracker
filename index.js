f157dbba8ace6040fb071d658a78419cd86bc83a
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-23 21:42:38 -0400

    added three more options in choices array; added if statement and called askMore; created if statements in askMore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cTable = require('console.table');
 const inquirer = require('inquirer');
 
 
-// Recieves information from table department
+// Recieves and displays all information from table
 const viewAll = (table) => {
     // Creates connection to the database
     const companyDb = mysql.createConnection(
@@ -19,11 +19,28 @@ const viewAll = (table) => {
     })
 };
 
+// Asks for additional information to insert into tables
+const askMore = (table) => {
+    if (table === 'add department') {
+        console.log(table);
+        // TODO: Add additional inquirer prompts and call module classes
+    } else if (table === 'add role') {
+        console.log(table);
+        // TODO: Add additional inquirer prompts and call module classes
+    } else {
+        console.log(table);
+        // TODO: Add additional inquirer prompts and call module classes
+    }
+}
+
 const init = () => {
     const choices = [
-        {name: 'View All Departments', value: 'department', short: 'View All Departments'},
-        {name: 'View All Roles', value: 'role', short: 'View All Roles'},
-        {name: 'View All Employees', value: 'employee', short: 'View All Employees'},
+        { name: 'View All Departments', value: 'department', short: 'View All Departments' },
+        { name: 'View All Roles', value: 'role', short: 'View All Roles' },
+        { name: 'View All Employees', value: 'employee', short: 'View All Employees' },
+        { name: 'Add a Department', value: 'add department', short: 'Add a Department' },
+        { name: 'Add a Role', value: 'add role', short: 'Add a Role' },
+        { name: 'Add an Employee', value: 'add employee', short: 'Add an Employee' },
         'Quit'
     ];
     inquirer.prompt([
@@ -36,9 +53,13 @@ const init = () => {
     ])
         .then((answer) => {
             if (answer.action === 'Quit') return '';
+            console.log(answer);
             const index = choices.findIndex((choice) => choice.value === answer.action);
+            console.log(index);
             // View All choices
             if (index >= 0 && index <= 2) viewAll(answer.action);
+            // Ask additional questions
+            if (index >= 3 && index <= 5) askMore(answer.action);
         })
         .catch((err) => console.log(err));
 };
