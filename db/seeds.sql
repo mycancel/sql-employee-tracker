063e6239ee701d874088ed7af905d95355ab4806
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-26 16:40:43 -0400

    removed ids from seeds.sql; Added Auto increment to primary id in schema.sql; defined foreign keys in tables in schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,32 +1,32 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES
-    ( 001, "Engineering"),
-    ( 002, "Finance"),
-    ( 003, "Legal"),
-    ( 004, "Sales");
+    ( "Engineering" ),
+    ( "Finance" ),
+    ( "Legal" ),
+    ( "Sales" );
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
-    ( 001, "Software Engineer", 130000, 001),
-    ( 002, "Lead Engineer", 160000, 001),
-    ( 003, "Accountant", 130000, 002),
-    ( 004, "Account Manager", 165000, 002),
-    ( 005, "Legal Team Lead", 255000, 003),
-    ( 006, "Lawyer", 200000, 003),
-    ( 007, "Sales Lead", 110000, 004),
-    ( 008, "Salesperson", 85000, 004);
+    ( "Software Engineer", 130000, 001 ),
+    ( "Lead Engineer", 160000, 001 ),
+    ( "Accountant", 130000, 002 ),
+    ( "Account Manager", 165000, 002 ),
+    ( "Legal Team Lead", 255000, 003 ),
+    ( "Lawyer", 200000, 003 ),
+    ( "Sales Lead", 110000, 004 ),
+    ( "Salesperson", 85000, 004 );
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ( 001, "Sally", "Edwin", 001, 004),
-    ( 002, "Samuel", "Edwards", 001, 004),
-    ( 003, "Susan", "Ellis", 001, 004),
-    ( 004, "Lee", "Evans", 002, null),
-    ( 005, "Amy", "Adams", 003, 006),
-    ( 006, "Andrew", "Madison", 004, null),
-    ( 007, "Laura", "Thomas", 005, null),
-    ( 008, "Lawrence", "Lawyer", 006, 007),
-    ( 009, "Lance", "Lee", 006, 007),
-    ( 010, "Sal", "Lewis", 007, null),
-    ( 011, "Sam", "Peters", 007, 010),
-    ( 012, "Scott", "Phillips", 007, 010);
+    ( "Sally", "Edwin", 001, 004 ),
+    ( "Samuel", "Edwards", 001, 004 ),
+    ( "Susan", "Ellis", 001, 004 ),
+    ( "Lee", "Evans", 002, null ),
+    ( "Amy", "Adams", 003, 006 ),
+    ( "Andrew", "Madison", 004, null ),
+    ( "Laura", "Thomas", 005, null ),
+    ( "Lawrence", "Lawyer", 006, 007 ),
+    ( "Lance", "Lee", 006, 007 ),
+    ( "Sal", "Lewis", 007, null ),
+    ( "Sam", "Peters", 007, 010 ),
+    ( "Scott", "Phillips", 007, 010 );
